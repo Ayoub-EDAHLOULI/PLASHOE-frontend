9e40656804ae9a86647c792120cc6c68e2a9a333
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-06-18 20:55:35 +0200

    Create the infinite text 'Mastery is a never-ending exploration'

---
+++ src/components/InfiniteText/InfiniteText.jsx
@@ -0,0 +1,25 @@
+import "./InfiniteText.scss";
+
+const InfiniteText = () => {
+  return (
+    <div className="infinite-text-container">
+      <div className="infinite-text">
+        <span className="odd">Mastery is a never-ending exploration</span>
+        <span className="even">Mastery is a never-ending exploration</span>
+        <span className="odd">Mastery is a never-ending exploration</span>
+        <span className="even">Mastery is a never-ending exploration</span>
+        <span className="odd">Mastery is a never-ending exploration</span>
+        <span className="even">Mastery is a never-ending exploration</span>
+        <span className="odd">Mastery is a never-ending exploration</span>
+        <span className="even">Mastery is a never-ending exploration</span>
+        <span className="odd">Mastery is a never-ending exploration</span>
+        <span className="even">Mastery is a never-ending exploration</span>
+        <span className="odd">Mastery is a never-ending exploration</span>
+        <span className="even">Mastery is a never-ending exploration</span>
+        <span className="odd">Mastery is a never-ending exploration</span>
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteText;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import AboutUs from "../components/About Us/AboutUs";
 import Companies from "../components/Companies/Companies";
 import Hero from "../components/Hero/Hero";
+import InfiniteText from "../components/InfiniteText/InfiniteText";
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <Hero />
       <Companies />
       <AboutUs />
+      <InfiniteText />
     </>
   );
 };
