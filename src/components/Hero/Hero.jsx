6fb93378ef5517129926bc804d214feabfc61cf8
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-06-18 18:17:44 +0200

    Create the Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import "./Hero.scss";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero_container">
+        <h1 className="hero_title">Welcome to the world of the future</h1>
+        <p className="hero_subtitle">
+          Make your move more comfortable with our products
+        </p>
+        <button className="hero_btn">Explore More</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
