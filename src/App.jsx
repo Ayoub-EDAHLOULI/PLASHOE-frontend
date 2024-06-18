6faaa45a3c9917e751f322898c8d4db35b5e2284
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-06-18 15:31:49 +0200

    Create the Carousel component with an infinite animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Carousel } from "./components/Carousel/Carousel";
 
 function App() {
   return (
     <>
-      <Home />
+      <div className="App">
+        <Carousel />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,39 @@
+import "./Carousel.scss";
+
+export function Carousel() {
+  return (
+    <>
+      <div className="carousel">
+        <div className="carousel_container">
+          <h6>Free standard shipping on all orders</h6>
+          <h6>
+            Discount: Use the code IMPACT20 to receive a 20% discount on your
+            order
+          </h6>
+          <h6>A question? Visit our contact page to send us a message</h6>
+          <h6>New Collaboration: Master & Dynamic x Paris Saint-Germain</h6>
+
+          {/* Diplucate the content */}
+
+          <h6>Free standard shipping on all orders</h6>
+          <h6>
+            Discount: Use the code IMPACT20 to receive a 20% discount on your
+            order
+          </h6>
+          <h6>A question? Visit our contact page to send us a message</h6>
+          <h6>New Collaboration: Master & Dynamic x Paris Saint-Germain</h6>
+
+          {/* Diplucate the content */}
+
+          <h6>Free standard shipping on all orders</h6>
+          <h6>
+            Discount: Use the code IMPACT20 to receive a 20% discount on your
+            order
+          </h6>
+          <h6>A question? Visit our contact page to send us a message</h6>
+          <h6>New Collaboration: Master & Dynamic x Paris Saint-Germain</h6>
+        </div>
+      </div>
+    </>
+  );
+}
