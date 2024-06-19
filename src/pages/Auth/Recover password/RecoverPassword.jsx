02298197910103d533d64f6f3673a3a6e0c9121d
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-06-20 00:12:00 +0200

    Add the formik logic to Recovered password

--- src/pages/Auth/Recover password/RecoverPassword.jsx
+++ src/pages/Auth/Recover password/RecoverPassword.jsx
@@ -1,13 +1,51 @@
 import "./RecoverPassword.scss";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function RecoverPassword() {
+  // Formik Logic
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+
+    // Validation Schema with Yup
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+    }),
+
+    // Schema for form submission
+    onSubmit: (values) => {
+      console.log("values", values);
+    },
+  });
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="recoverPassword">
       <div className="recoverPassword_container">
         <h1>Recover Password</h1>
-        <form>
-          <input type="email" placeholder="Enter your email" />
+        <form onSubmit={handleSubmit}>
+          <div className="labelError">
+            {formik.touched.email && formik.errors.email ? (
+              <p>{formik.errors.email}</p>
+            ) : null}
+          </div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
           <button type="submit">Recover password</button>
           <Link to="/login" className="remember_password">
             Remembered your password?
