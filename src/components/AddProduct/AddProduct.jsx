d20acd8a738d673b362fb6f44c482b1bd2285836
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-06-25 20:06:43 +0200

    Create the uploadImage function to save product image in public folder and the path to that image in server. I faced a problem of uploadign that image seuccessfully i worked for this bug in the end i switched from fetch to axios so it's worked actually i don't know why

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,21 +1,79 @@
 import "./AddProduct.scss";
+import { useState } from "react";
+import axios from "axios";
 
 function AddProduct() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [file, setFile] = useState(null);
+
+  //Upload image to server and get the URL
+  //Upload Image
+  const uploadImage = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const res = await axios.post(
+        "http://127.0.0.1:3000/api/v1/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      return res.data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    const product = {
+      name,
+      description,
+      price,
+      quantity,
+      image: await uploadImage(file),
+    };
+
+    console.log(product);
+  };
+
   return (
     <div className="add-product">
       <h2>Add Product</h2>
-      <form>
+      <form onSubmit={handelSubmit}>
         <div className="form-group">
           <label htmlFor="product-name">Product Name</label>
-          <input type="text" name="product-name" className="input-strings" />
+          <input
+            type="text"
+            name="product-name"
+            className="input-strings"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="product-description">Product Description</label>
-          <textarea name="product-description"></textarea>
+          <textarea
+            name="product-description"
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+          ></textarea>
         </div>
         <div className="form-group">
           <label htmlFor="product-price">Product Price</label>
-          <input type="text" name="product-price" className="input-strings" />
+          <input
+            type="text"
+            name="product-price"
+            className="input-strings"
+            onChange={(e) => setPrice(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="product-quantity">Product Quantity</label>
@@ -23,11 +81,19 @@ function AddProduct() {
             type="text"
             name="product-quantity"
             className="input-strings"
+            onChange={(e) => setQuantity(e.target.value)}
           />
         </div>
         <div className="form-group">
           <label htmlFor="product-image">Product Image</label>
-          <input type="file" name="product-image" className="input-image" />
+          <input
+            type="file"
+            name="image"
+            accept="image/*"
+            className="input-image"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <button onClick={uploadImage}>Upload Image</button>
         </div>
         <button type="submit">Add Product</button>
       </form>
